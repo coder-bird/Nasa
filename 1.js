89e9d433f651c427e8c93724660c96b1f9ec54d2
Saumya <saumya.1126@gmail.com>
2020-10-04 15:34:29 +0530

    New changes

--- 1.js
+++ 1.js
@@ -1,10 +1,15 @@
-function myMove() {
-  var elem = document.getElementById("myAnimation");   
+function myMove1() {
+    var elem = document.getElementById("myAnimation");
+    elem.style.backgroundSize = '300px';
+    elem.style.backgroundImage = 'url("https://www.uokpl.rs/fpng/f/430-4305418_international-space-station.png")';
   var pos = 350;
   var id = setInterval(frame, 10);
   function frame() {
+    document.getElementById("myContainer").style.backgroundImage = "";
     if (pos == 0) {
       clearInterval(id);
+      elem.style.backgroundImage = "";
+      document.getElementById("myContainer").style.backgroundImage = 'url("https://www.uokpl.rs/fpng/f/430-4305418_international-space-station.png")';
     } else {
       pos--; 
       elem.style.top = pos + 'px'; 
@@ -12,3 +17,87 @@ function myMove() {
     }
   }
 }
+/*function disp1(){
+  document.getElementById("text7").style.visibility = "hidden";
+  document.getElementById("text6").style.visibility = "hidden";
+  document.getElementById("text5").style.visibility = "hidden";
+  document.getElementById("text4").style.visibility = "hidden";
+  document.getElementById("text3").style.visibility = "hidden";
+  document.getElementById("text2").style.visibility = "hidden";
+  document.getElementById("sci-image").style.backgroundImage = 'url("coast.jpg")';
+  document.getElementById("sci-image").style.backgroundSize = '500px';
+  document.getElementById("text1").style.visibility = "visible";
+  console.log("hi");
+}
+function disp2(){
+  document.getElementById("text7").style.visibility = "hidden";
+  document.getElementById("text6").style.visibility = "hidden";
+  document.getElementById("text5").style.visibility = "hidden";
+  document.getElementById("text4").style.visibility = "hidden";
+  document.getElementById("text3").style.visibility = "hidden";
+  document.getElementById("text1").style.visibility = "hidden";
+  document.getElementById("sci-image").style.backgroundImage = 'url("climate.jpg")';
+  document.getElementById("sci-image").style.backgroundSize = '500px';
+  document.getElementById("text2").style.visibility = "visible";
+  console.log("hi");
+}
+function disp3(){
+  document.getElementById("text7").style.visibility = "hidden";
+  document.getElementById("text6").style.visibility = "hidden";
+  document.getElementById("text5").style.visibility = "hidden";
+  document.getElementById("text4").style.visibility = "hidden";
+  document.getElementById("text2").style.visibility = "hidden";
+  document.getElementById("text1").style.visibility = "hidden";
+  document.getElementById("sci-image").style.backgroundImage = 'url("flood.jpg")';
+  document.getElementById("sci-image").style.backgroundSize = '500px';
+  document.getElementById("text3").style.visibility = "visible";
+  console.log("hi");
+}
+function disp4(){
+  document.getElementById("text7").style.visibility = "hidden";
+  document.getElementById("text6").style.visibility = "hidden";
+  document.getElementById("text5").style.visibility = "hidden";
+  document.getElementById("text3").style.visibility = "hidden";
+  document.getElementById("text2").style.visibility = "hidden";
+  document.getElementById("text1").style.visibility = "hidden";
+  document.getElementById("sci-image").style.backgroundImage = 'url("venice.jpg")';
+  document.getElementById("sci-image").style.backgroundSize = '500px';
+  document.getElementById("text4").style.visibility = "visible";
+  console.log("hi");
+}
+function disp5(){
+  document.getElementById("text7").style.visibility = "hidden";
+  document.getElementById("text6").style.visibility = "hidden";
+  document.getElementById("text4").style.visibility = "hidden";
+  document.getElementById("text3").style.visibility = "hidden";
+  document.getElementById("text2").style.visibility = "hidden";
+  document.getElementById("text1").style.visibility = "hidden";
+  document.getElementById("sci-image").style.backgroundImage = 'url("dakota.jpg")';
+  document.getElementById("sci-image").style.backgroundSize = '500px';
+  document.getElementById("text5").style.visibility = "visible";
+  console.log("hi");
+}
+function disp6(){
+  document.getElementById("text7").style.visibility = "hidden";
+  document.getElementById("text5").style.visibility = "hidden";
+  document.getElementById("text4").style.visibility = "hidden";
+  document.getElementById("text3").style.visibility = "hidden";
+  document.getElementById("text2").style.visibility = "hidden";
+  document.getElementById("text1").style.visibility = "hidden";
+  document.getElementById("sci-image").style.backgroundImage = 'url("dakota.jpg")';
+  document.getElementById("sci-image").style.backgroundSize = '500px';
+  document.getElementById("text6").style.visibility = "visible";
+  console.log("hi");
+}
+function disp7(){
+  document.getElementById("text6").style.visibility = "hidden";
+  document.getElementById("text5").style.visibility = "hidden";
+  document.getElementById("text4").style.visibility = "hidden";
+  document.getElementById("text3").style.visibility = "hidden";
+  document.getElementById("text2").style.visibility = "hidden";
+  document.getElementById("text1").style.visibility = "hidden";
+  document.getElementById("sci-image").style.backgroundImage = 'url("sea.jpg")';
+  document.getElementById("sci-image").style.backgroundSize = '500px';
+  document.getElementById("text7").style.visibility = "visible";
+  console.log("hi");
+}*/
